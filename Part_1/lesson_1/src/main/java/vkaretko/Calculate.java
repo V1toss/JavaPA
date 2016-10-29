e6e17b5fa9d2754ea2cb94397732872a39bc5bfb
V1toss <C:\Users\Vitoss\AppData\Roaming\The Bat!>
2016-10-30 00:51:12 +0300

    1st lesson

---
+++ Part_1/lesson_1/src/main/java/vkaretko/Calculate.java
@@ -0,0 +1,15 @@
+package vkaretko;
+
+/**
+ * Часть 1. Базовый синтаксис.
+ * Урок 1. Создание Hello World
+ *
+ * @author Karetko Victor
+ * @version 1.00
+ */
+
+public class Calculate {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
