e94b3e1d709c9c7517a47b424daa6c26babe4250
V1toss <vitoss@inbox.ru>
2016-10-31 17:03:38 +0300

    31.10.2016 added Task 4.1

---
+++ part_1/lesson_4/src/main/java/vkaretko/Task_4_1/Square.java
@@ -0,0 +1,52 @@
+package vkaretko.Task_4_1;
+
+/**
+ * Часть 1. Базовый синтаксис.
+ * Урок 4.1. Создать программу вычисляющую формулу квадратного уравнения
+ * Класс для создания объекта типа "Square".
+ *
+ * @author Karetko Victor
+ * @version 1.00
+ * @since 31.10.2016
+ */
+public class Square {
+
+    private float a, b, c;
+
+    /**
+     * Конструктор класса Square
+     *
+     * @param a значение a в формуле квадратного уравнения
+     * @param a значение b в формуле квадратного уравнения
+     * @param a значение c в формуле квадратного уравнения
+     */
+    public Square (float a, float b, float c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Метод для подсчёта квадратного уравнения
+     *
+     * @param x значение x в формуле квадратного уравнения
+     * @return результат вычисления по формуле квадратного уравнения
+     */
+    public float calculate (int x) {
+        return (float) ((this.a*Math.pow(x,2)) + (this.b * x) + this.c);
+    }
+
+    /**
+     * Метод для задания диапазон параметров "x" для выполнения расчётов
+     * по формуле квадратного уравнения и вывода результатов на консоль
+     *
+     * @param start начальное значение "x"
+     * @param finish конечное значение "x"
+     * @param step шаг увеличениz значения "x" в диапазоне от start до finish
+     */
+    public void show (int start, int finish, int step) {
+        for (int i = start; i <= finish; i+= step) {
+            System.out.println(calculate(i));
+        }
+    }
+}
---
+++ part_1/lesson_4/src/test/java/vkaretko/Task_4_1/SquareTest.java
@@ -0,0 +1,42 @@
+package vkaretko.Task_4_1;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тест-класс для тестирования объекта "Square".
+ *
+ * @author Karetko Victor
+ * @version 1.00
+ * @since 31.10.2016
+ */
+public class SquareTest {
+    @Test
+    public void whenSquareCalculateWithArgumentThreeThenResultEighteen() {
+        Square sq = new Square(1f,2f,3f);
+        assertThat(sq.calculate(3), is(18f));
+    }
+
+    @Test
+    public void whenSquareShowWithArgumentsOneAndFiveAndTwoThenPrintOutSixAndEighteenAndThirtyEight() {
+        Square sq = new Square(1f,2f,3f);
+        String result = null;
+        PrintStream outConsole = System.out;
+        try {
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(outStream);
+            System.setOut(printStream);
+            sq.show(1,5,2);
+            printStream.flush();
+            result = outStream.toString();
+        } finally {
+            System.setOut(outConsole);
+        }
+        assertThat(result, is("6.0\r\n18.0\r\n38.0\r\n"));
+    }
+}
