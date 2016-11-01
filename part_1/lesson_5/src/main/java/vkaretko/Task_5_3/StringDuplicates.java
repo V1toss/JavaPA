a6c45031964af9181b46809f45ac75278cb94d9e
V1toss <vitoss@inbox.ru>
2016-11-01 19:27:58 +0300

    01.11.2016 Added Task 1.5.3

--- part_1/lesson_5/src/main/java/vkaretko/Task_5_3/StringDuplicates.java
+++ part_1/lesson_5/src/main/java/vkaretko/Task_5_3/StringDuplicates.java
@@ -20,7 +20,7 @@ public class StringDuplicates {
     public String[] remove (String[] array) {
 
         int countDuplicates = 0;
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length-1; i++) {
             if (array[i] != null) {
                 for (int j = i + 1; j < array.length; j++) {
                     if (array[i].equals(array[j])) {
