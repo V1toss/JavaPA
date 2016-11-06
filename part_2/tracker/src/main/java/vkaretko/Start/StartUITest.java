58746ee25f3cdbf96f147a076ce23bef17385677
V1toss <vitoss@inbox.ru>
2016-11-06 10:34:48 +0300

    06.11.2016 updated StartUITest

--- part_2/tracker/src/main/java/vkaretko/Start/StartUITest.java
+++ part_2/tracker/src/main/java/vkaretko/Start/StartUITest.java
@@ -9,12 +9,51 @@ package vkaretko.Start;
  */
 public class StartUITest {
     /**
-     * Initialization method     *
+     * Initialization method for simulating user Actions:
+     * 1. Add new item
+     * 2. Show result
+     * 3. Edit item and show result
+     * 4. Add new item, delete first item and show result
+     * 5. Find item by Id
+     * 6. Add comment and show all
+     * 7. Exit program
      * @param args arguments from command line
      */
     public static void main(String[] args) {
-        Input input = new StubInput(new String[] {"create stub task"});
+
         Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[] {
+                "0", "name", "desc", "y"});
+        new StartUI(input, tracker).init();
+        String id = tracker.getAll()[0].getId();
+
+        input = new StubInput(new String[] {
+                "1", "y"});
+        new StartUI(input, tracker).init();
+
+        input = new StubInput(new String[] {
+                "2", id, "name after edit", "desc after edit", "n", "1", "y"});
+        new StartUI(input, tracker).init();
+
+        input = new StubInput(new String[] {
+                "0", "second item", "description", "n", "3", id, "n", "1", "y"});
+        new StartUI(input, tracker).init();
+
+        input = new StubInput(new String[] {
+                "0", "second item", "description", "n", "3", id, "n", "1", "y"});
+        new StartUI(input, tracker).init();
+        id = tracker.getAll()[0].getId();
+
+        input = new StubInput(new String[] {
+                "4", id, "y"});
+        new StartUI(input, tracker).init();
+
+        input = new StubInput(new String[] {
+                "6", id, "test comment", "n", "1", "y"});
+        new StartUI(input, tracker).init();
+
+        input = new StubInput(new String[] {
+                "7"});
         new StartUI(input, tracker).init();
     }
 }
