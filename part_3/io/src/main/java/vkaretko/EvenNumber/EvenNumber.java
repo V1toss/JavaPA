73be3481c903f7c7805bd0e996ac1c5aa44dc3b1
V1toss <vitoss@inbox.ru>
2016-11-14 00:04:29 +0300

    13.11.2016 updated EvenNumberTest

--- part_3/io/src/main/java/vkaretko/EvenNumber/EvenNumber.java
+++
@@ -1,7 +0,0 @@
-package vkaretko.EvenNumber;
-
-/**
- * Created by Vitoss on 13.11.2016.
- */
-public class EvenNumber {
-}
--- part_3/io/src/test/java/vkaretko/EvenNumberTest.java
+++ part_3/io/src/test/java/vkaretko/EvenNumberTest.java
@@ -1,7 +1,36 @@
 package vkaretko;
 
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * Created by Vitoss on 13.11.2016.
+ * Test class for EvenNumber.
+ *
+ * @author Karetko Victor
+ * @version 1.00
+ * @since 13.11.2016
  */
 public class EvenNumberTest {
+    /**
+     * Test-method for checking even numbers.
+     */
+    @Test
+    public void whenEvenNumberTwoInStreamThenResultTrue() {
+        EvenNumber number = new EvenNumber();
+        ByteArrayInputStream stream = new ByteArrayInputStream("2".getBytes());
+        assertThat(number.isNumber(stream), is(true));
+    }
+
+    /**
+     * Test-method for checking odd numbers.
+     */
+    @Test
+    public void whenOddNumberThreeInStreamThenResultFalse() {
+        EvenNumber number = new EvenNumber();
+        ByteArrayInputStream stream = new ByteArrayInputStream("3".getBytes());
+        assertThat(number.isNumber(stream), is(false));
+    }
 }
