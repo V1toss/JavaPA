31d4057c9977b2000aaf55de5bfde460506c0ce0
V1toss <vitoss@inbox.ru>
2017-02-16 00:45:32 +0300

    add task 8.1.1

---
+++ part_8_sql/basic/src/main/java/vkaretko/create.sql
@@ -0,0 +1,49 @@
+﻿CREATE DATABASE java_a_from_z;
+
+CREATE TABLE roles (
+    role_id serial PRIMARY KEY,
+    role_name CHARACTER VARYING (200)
+);
+
+CREATE TABLE rights (
+    right_id serial PRIMARY KEY,
+    right_name CHARACTER VARYING (200),
+    role_id INTEGER REFERENCES roles(role_id)
+);
+CREATE TABLE users (
+    user_id serial PRIMARY KEY,
+    login CHARACTER VARYING (200),
+    password CHARACTER VARYING (200),
+    create_date TIMESTAMP,
+    role_id INTEGER REFERENCES roles(role_id)
+);
+
+CREATE TABLE category (
+    cat_id serial PRIMARY KEY,
+    cat_name  CHARACTER VARYING (200)
+);
+
+CREATE TABLE statuses (
+    stat_id serial PRIMARY KEY,
+    stat_name CHARACTER VARYING (200)
+);
+
+CREATE TABLE items (
+    item_id serial PRIMARY KEY,
+    date TIMESTAMP,
+    stat_id INTEGER REFERENCES statuses(stat_id),
+    cat_id INTEGER REFERENCES category(cat_id),    
+    user_id INTEGER REFERENCES users(user_id)
+);
+
+CREATE TABLE comments (
+    comment_id serial PRIMARY KEY,
+    comment  CHARACTER VARYING (1000),
+    item_id INTEGER REFERENCES items(item_id)
+);
+
+CREATE TABLE att_files (
+    file_id serial PRIMARY KEY,
+    file_path  CHARACTER VARYING (1000),
+    item_id INTEGER REFERENCES items(item_id)
+);
---
+++ part_8_sql/basic/src/main/java/vkaretko/insert.sql
@@ -0,0 +1,15 @@
+﻿INSERT INTO roles(role_name) values ('administrator');
+
+INSERT INTO rights(right_name, role_id) values ('full', 1);
+
+INSERT INTO users(login, password, create_date, role_id) values ('admin', '1234', '2016-01-08 04:05:06', 1);
+
+INSERT INTO category(cat_name) values ('task');
+
+INSERT INTO statuses(stat_name) values ('new');
+
+INSERT INTO items(date, stat_id, cat_id, user_id) values('2016-10-10 11:05:06', 1, 1, 1); 
+
+INSERT INTO comments(comment, item_id) values('test comment', 1);
+
+INSERT INTO att_files(file_path, item_id) values('c:\file.txt', 1);
