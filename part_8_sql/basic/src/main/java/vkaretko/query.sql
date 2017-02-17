a1c0f0e22ba16052ff19f9591a74ae1b3cb3b833
V1toss <vitoss@inbox.ru>
2017-02-17 16:10:03 +0300

    added task 8.2.1

---
+++ part_8_sql/basic/src/main/java/vkaretko/query.sql
@@ -0,0 +1,12 @@
+﻿-- select all items in October 2016
+SELECT i.item_id, i.date, s.stat_name, c.cat_name, u.login FROM items AS i 
+INNER JOIN statuses AS s ON i.stat_id = s.stat_id
+INNER JOIN category AS c ON i.cat_id = c.cat_id
+INNER JOIN users AS u ON i.user_id = u.user_id 
+WHERE i.date BETWEEN '2016-10-01 00:00:00' AND '2016-11-01 00:00:00';
+
+--select all comments for item with id 1
+SELECT c.comment, i.item_id, i.date, u.login FROM comments AS c
+INNER JOIN items AS i ON c.item_id = i.item_id
+INNER JOIN users AS u ON i.user_id = u.user_id
+WHERE c.item_id = 1;
