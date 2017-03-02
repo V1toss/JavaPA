1ef3ec4d2df4d7daafe1b880878c87f6be6896e4
V1toss <vitoss@inbox.ru>
2017-03-02 16:08:00 +0300

    02.03.2017 update UserDBServlet

--- part_9_servlets_jsp/userdbservlet/src/main/java/vkaretko/servlets/UserDBServlet.java
+++ part_9_servlets_jsp/userdbservlet/src/main/java/vkaretko/servlets/UserDBServlet.java
@@ -57,8 +57,8 @@ public class UserDBServlet extends HttpServlet {
             for (User user : dbMan.getAll()) {
                 writer.append(user.toString());
                 writer.append("<br/>");
+                writer.flush();
             }
-            writer.flush();
         }
     }
 
